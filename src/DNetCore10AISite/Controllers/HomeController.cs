0258fbddeed2bb82d998b93c3087552d04209b61
Terry Xu <v-tex@microsoft.com>
2018-04-25 01:35:06 -0700

    update

--- src/DNetCore10AISite/Controllers/HomeController.cs
+++ src/DNetCore10AISite/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.InternalAbstractions;
+using Microsoft.Extensions.DependencyModel;
 
 namespace DNetCore10AISite.Controllers
 {
@@ -16,6 +20,14 @@ namespace DNetCore10AISite.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            var runtimeId = RuntimeEnvironment.GetRuntimeIdentifier();
+            var assemblies = DependencyContext.Default.GetRuntimeAssemblyNames(runtimeId);
+
+            ViewBag.Assemblies = assemblies;
+
+            ViewBag.Modules = Process.GetCurrentProcess().Modules;
+
+            ViewBag.EnvironmentVariables = Environment.GetEnvironmentVariables();
 
             return View();
         }
@@ -30,6 +42,21 @@ namespace DNetCore10AISite.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        private async void GetResponseStream()
+        {
+            var req = WebRequest.Create("http://bing.com");
+            var response = await req.GetResponseAsync();
+
+            using (var s = response.GetResponseStream())
+            {
+                byte[] content = new byte[512];
+                s.Read(content, 0, content.Length);
+                var contentS = System.Text.Encoding.UTF8.GetString(content);
+                Console.WriteLine(contentS);
+                ViewBag.Content = contentS;
+            }
         }
     }
 }
